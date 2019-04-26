3364fc028923395592db93d1f69932af88d62a5d
lambdadroid <lambdadroid@gmail.com>
2019-04-26 20:31:37 +0200

    Add SAFE_MODE option to guard code that updates device info
    Signed-off-by: geckyn <geckyn@gmail.com>

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -2419,6 +2419,7 @@ continue_boot:
 
 void write_device_info_mmc(device_info *dev)
 {
+#ifndef SAFE_MODE
 	unsigned long long ptn = 0;
 	unsigned long long size;
 	int index = INVALID_PTN;
@@ -2459,6 +2460,9 @@ void write_device_info_mmc(device_info *dev)
 		dprintf(CRITICAL, "ERROR: Cannot write device info\n");
 		ASSERT(0);
 	}
+#else
+	dprintf(CRITICAL, "Ignoring attempt to write device info\n");
+#endif
 }
 
 void read_device_info_mmc(struct device_info *info)
@@ -2506,6 +2510,7 @@ void read_device_info_mmc(struct device_info *info)
 
 void write_device_info_flash(device_info *dev)
 {
+#ifndef SAFE_MODE
 	struct device_info *info = memalign(PAGE_SIZE, ROUNDUP(BOOT_IMG_MAX_PAGE_SIZE, PAGE_SIZE));
 	struct ptentry *ptn;
 	struct ptable *ptable;
@@ -2538,6 +2543,9 @@ void write_device_info_flash(device_info *dev)
 			return;
 	}
 	free(info);
+#else
+	dprintf(CRITICAL, "Ignoring attempt to write device info\n");
+#endif
 }
 
 static int read_allow_oem_unlock(device_info *dev)
@@ -2580,6 +2588,7 @@ static int read_allow_oem_unlock(device_info *dev)
 
 static int write_allow_oem_unlock(bool allow_unlock)
 {
+#ifndef SAFE_MODE
 	unsigned offset;
 	int index;
 	unsigned long long ptn;
@@ -2618,6 +2627,10 @@ static int write_allow_oem_unlock(bool allow_unlock)
 	}
 
 	return 0;
+#else
+	dprintf(CRITICAL, "Ignoring attempt to allow OEM unlock\n");
+	return -1;
+#endif
 }
 
 void read_device_info_flash(device_info *dev)
