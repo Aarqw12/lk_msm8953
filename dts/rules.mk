c09b89135a17d9ffcc4f42b27b03060431058762
geckyn <geckyn@gmail.com>
2020-06-28 21:48:18 +0000

    dts: add Samsung galaxy j4primelte(j4+) support
    Signed-off-by: geckyn <geckyn@gmail.com>

--- dts/rules.mk
+++ dts/rules.mk
@@ -18,5 +18,6 @@ endif
 ifeq ($(PROJECT), msm8952-secondary)
 DTBS += \
 	$(LOCAL_DIR)/msm8940-xiaomi-santoni.dtb \
-	$(LOCAL_DIR)/msm8917-sec-j6primelte-swa-open-r06.dtb
+	$(LOCAL_DIR)/msm8917-sec-j6primelte-swa-open-r06.dtb \
+	$(LOCAL_DIR)/msm8917-sec-j4primelte-sea-open-r06.dtb
 endif
